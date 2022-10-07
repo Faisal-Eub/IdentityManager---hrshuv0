d0db0ec32a3d59c83ae0f0669c25c5a46e70c2f6
hrshuv0 <h.r.shuvo9@gmail.com>
2022-10-07 21:39:25 +0600

    added register view

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using IdentityManager.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Register()
+    {
+        RegisterVm registerVm = new RegisterVm();
+        
+        return View(registerVm);
+    }
+}
---
+++ IdentityManager/Models/ViewModel/RegisterVm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models.ViewModel;
+
+public class RegisterVm
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 4, ErrorMessage = "The {0} must be {1} character long.")]
+    public string Password { get; set; }
+    
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password and Confirm Password Must be matched")]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPassword { get; set; }
+
+    public string? Name { get; set; }
+    
+}
